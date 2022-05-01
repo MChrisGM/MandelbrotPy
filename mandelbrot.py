8f22c2ab05e1952623f222ccb24431db26541f07
MChrisGM <c.i.cristakos@gmail.com>
2022-05-01 17:04:23 +0100

    Added threads Removed pygame window Added gitignore

--- mandelbrot.py
+++ mandelbrot.py
@@ -3,14 +3,15 @@ import numpy as np
 import os
 from PIL import Image
 
-game.init()
+from threading import Thread
 
 WIDTH = 500
 HEIGHT = 500
+FRAMES = 50
 
-screen = game.display.set_mode((WIDTH, HEIGHT))
-
-game.display.set_caption("Mandelbrot Set")
+frames = []
+threads = []
+remaining = [i for i in range(FRAMES)]
 
 generated_points = False 
 written_to_file = False
@@ -21,8 +22,6 @@ scroll_speed = 0.9
 speed = 30
 x_offset = -1.74999841099374081749002483162428393452822172335808534616943930976364725846655540417646727085571962736578151132907961927190726789896685696750162524460775546580822744596887978637416593715319388030232414667046419863755743802804780843375
 y_offset = -0.00000000000000165712469295418692325810961981279189026504290127375760405334498110850956047368308707050735960323397389547038231194872482690340369921750514146922400928554011996123112902000856666847088788158433995358406779259404221904755
-
-last_frame = 0
 
 iterations = 500
 
@@ -43,10 +42,8 @@ def num_to_rgb(num):
 def calc_colour(point):
     if point.imag**2 + point.real**2 > 4:
         return (255, 255, 255)
-
     def next_z(zn, c):
         return zn**2 + c
-    
     z = 0
     z_count = 0
     for x in range(iterations):
@@ -59,45 +56,59 @@ def calc_colour(point):
         except OverflowError:
             col = _map(z_count, 0, iterations-1, 0, 16777215)
             return num_to_rgb(col)
-    
     return (0, 0, 0)
 
-a = 0
-num = 0
-running = True
-while running:
-    for event in game.event.get():
-        if event.type == game.QUIT:
-            running = False
-            
-    screen.fill((255, 255, 255))
+def calculate_indices():
+    scrollarray = []
+    s = scroll
+    for i in range(FRAMES):
+        s*=scroll_speed
+        scrollarray.append((i, s))
+    return scrollarray
 
-    if not generated_points:
-        mandelbrot = Image.new("RGB", (WIDTH, HEIGHT))
-        for x in range(WIDTH):
-            for y in range(HEIGHT):
-                mapped_x = _map(x, 0, WIDTH, -scroll*(WIDTH/HEIGHT)+x_offset, scroll*(WIDTH/HEIGHT)+x_offset)
-                mapped_y = _map(y, 0, HEIGHT, -scroll+y_offset, scroll+y_offset)
+def calculate_remaining(n):
+    global remaining
+    remaining.remove(n)
+    return len(remaining)
 
-                colour = calc_colour(complex(mapped_x, mapped_y))
-                points[y][x] = colour
+def task(n, s):
+    mandelbrot = Image.new("RGB", (WIDTH, HEIGHT))
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+            mapped_x = _map(x, 0, WIDTH, -s*(WIDTH/HEIGHT)+x_offset, s*(WIDTH/HEIGHT)+x_offset)
+            mapped_y = _map(y, 0, HEIGHT, -s+y_offset, s+y_offset)
+            colour = calc_colour(complex(mapped_x, mapped_y))
+            mandelbrot.putpixel((x, y), colour)
 
-                mandelbrot.putpixel((x, y), colour)
+    print('Finished frame ', n)
+    r = calculate_remaining(n)
+    print('Remaining: '+str(r))
+    frames.append((n, mandelbrot))
 
-                screen.set_at((x, y), colour)
-            if a%10 == 0: print(f"{(x*HEIGHT)+y+1}/{WIDTH*HEIGHT}")
-            a += 1
+def save_frames():
+    for i in frames:
+        i[1].save(f"./mandelbrot/mandelbrot_img{i[0]}.png")
 
-        mandelbrot.save(f"mandelbrot/mandelbrot_img{num}.png")
-        # generated_points = True
-        print(f"done {num}")
-        num += 1
-        scroll *= scroll_speed
-        # os.system("shutdown -s -t 10")
-        
-    else:
-        for x in range(WIDTH):
-            for y in range(HEIGHT):
-                screen.set_at((x, y), points[y][x])
+def create_threads():
+    r = calculate_indices()
+    for i in range(FRAMES):
+        print('Creating thread ',i)
+        t = Thread(target=task, args=(r[i][0],r[i][1]))
+        threads.append(t)
 
-    game.display.update()
+def start_threads():
+    for i in range(len(threads)):
+        print('Starting thread ',i)
+        threads[i].start()
+
+def join_threads():
+    for t in threads:
+        t.join()
+    print('Done')
+
+
+if __name__ == '__main__':
+    create_threads()
+    start_threads()
+    join_threads()
+    save_frames()
